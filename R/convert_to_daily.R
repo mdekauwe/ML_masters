7f2ee2efafa293a3c1aab87fc53bb230d81266bc
mdekauwe <mdekauwe@gmail.com>
2025-02-27 09:42:42 +0000

    added random forest

--- R/convert_to_daily.R
+++ R/convert_to_daily.R
@@ -16,30 +16,33 @@ convert_to_daily <- function(df) {
   MOL_C_TO_GRAMS_C <- 12.0
   SEC_2_HLFHOUR <- 1800.
   WM2_TO_KG_M2_S <- 1.0 / ( 2.45 * 1E6 )
+  PA_2_KPA = 0.001
+  HPA_2_KPA = 0.1
   
   df <- df %>%
     mutate(
       GPP = GPP * UMOL_TO_MOL * MOL_C_TO_GRAMS_C * SEC_2_HLFHOUR,
       Precip = Precip * SEC_2_HLFHOUR,
-      ET = Qle * WM2_TO_KG_M2_S * SEC_2_HLFHOUR
+      ET = Qle * WM2_TO_KG_M2_S * SEC_2_HLFHOUR,
+      VPD = VPD * HPA_2_KPA
     )
   
   df$date <- ymd_hms(df$date)
   
   # Resample to daily 
   df <- df %>%
-    mutate(date = floor_date(date, "day")) %>%  # Round down to the nearest day
+    mutate(date = floor_date(date, "day")) %>%  
     group_by(date) %>%
     summarise(
-      GPP = sum(GPP, na.rm = TRUE), 
-      ET = sum(ET, na.rm = TRUE),  
-      Precip = sum(Precip, na.rm = TRUE),
-      Qle = mean(Qle, na.rm = TRUE),
-      Tair = mean(Tair, na.rm = TRUE),
-      SW_rad = mean(SW_rad, na.rm = TRUE),
-      Rn = mean(Rn, na.rm = TRUE),
-      Pa = mean(Pa, na.rm = TRUE),
-      VPD = mean(VPD, na.rm = TRUE)
+      GPP = sum(GPP, na.rm=TRUE), 
+      ET = sum(ET, na.rm=TRUE),  
+      Precip = sum(Precip, na.rm=TRUE),
+      Qle = mean(Qle, na.rm=TRUE),
+      Tair = mean(Tair, na.rm=TRUE),
+      SW_rad = mean(SW_rad, na.rm=TRUE),
+      Rn = mean(Rn, na.rm=TRUE),
+      Pa = mean(Pa, na.rm=TRUE),
+      VPD = mean(VPD, na.rm=TRUE)
     )
   
   return(df)
